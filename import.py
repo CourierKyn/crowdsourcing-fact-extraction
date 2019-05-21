b5dc25699c8ee07b6fdf6b4fdbadd5bd62b9fadd
Franklin Li <franklin_a_h_p@qq.com>
2019-05-21 21:24:51 +0800

    添加 import 模板

---
+++ import.py
@@ -0,0 +1,14 @@
+from IPython.display import display
+import pandas as pd
+pd.options.display.max_rows = 10
+import random
+import re
+import numpy as np
+import matplotlib, matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+matplotlib.rcParams['font.sans-serif'] = ['Arial Unicode MS', 'sans-serif']
+import pprint
+import os
+from bs4 import BeautifulSoup
+import json
